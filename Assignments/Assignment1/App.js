8551cb1c348e921c0d6c195ad659fa590cf16e3c
anshulyerudkar1 <parshwanath.ornaments.1008@gmail.com>
2026-08-08 23:29:57 +0530

    feat: initialize project with basic React setup and rendering logic

--- Assignments/Assignment1/App.js
+++ Assignments/Assignment1/App.js
@@ -1,5 +1,15 @@
-const root = document.getElementById("root");
+// const root = document.getElementById("root");
 
-const heading = document.createElement("h1");
-heading.innerHTML = "Hellow world";
-root.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hellow world";
+// root.appendChild(heading);
+
+////// React
+
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement("h1", null, "Hellow world");
+root.render(heading);
