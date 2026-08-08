226d5e5b303adf18317d7473695ac1d5ad99ec28
anshulyerudkar1 <parshwanath.ornaments.1008@gmail.com>
2026-08-09 00:16:11 +0530

    feat: initialize project with basic React setup and render nested elements

--- Assignments/Assignment1/App.js
+++ Assignments/Assignment1/App.js
@@ -6,10 +6,20 @@
 
 ////// React
 
-import ReactDOM from "react-dom/client";
-import React from "react";
+// import ReactDOM from "react-dom/client";
+// import React from "react";
+
+const heading = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "h1",
+        { id: "heading" },
+        "Hellow world from React")
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading = React.createElement("h1", null, "Hellow world");
+
+console.log(heading); //  -> it will return an object not an HTML element
 root.render(heading);
