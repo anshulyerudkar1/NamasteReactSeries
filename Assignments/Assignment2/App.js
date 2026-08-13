874b1b03ea0eea3f487419c43be3736a4f3c7067
anshulyerudkar1 <parshwanath.ornaments.1008@gmail.com>
2026-08-14 02:10:20 +0530

    chore: update node_modules and dependency artifacts across project directories

---
+++ Assignments/Assignment2/App.js
@@ -0,0 +1,25 @@
+// const root = document.getElementById("root");
+
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hellow world";
+// root.appendChild(heading);
+
+////// React
+
+// import ReactDOM from "react-dom/client";
+// import React from "react";
+
+const heading = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "h1",
+        { id: "heading" },
+        "Hellow world from React")
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+console.log(heading); //  -> it will return an object not an HTML element
+root.render(heading);
