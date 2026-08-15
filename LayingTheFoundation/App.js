4857088673362212a4e074b54061c6a576ae2717
anshulyerudkar1 <parshwanath.ornaments.1008@gmail.com>
2026-08-15 11:57:58 +0530

    feat: initialize App.js with React functional components and element rendering examples

--- LayingTheFoundation/App.js
+++ LayingTheFoundation/App.js
@@ -4,7 +4,56 @@ import React from "react";
 
 // React Element
 
-const heading = React.createElement("h1", { id: "heading" }, "Hellow world from React");
+// const heading = React.createElement("h1", { id: "heading" }, "Hellow world from React");
+
+// const jsxHedding = <h1 id="heading">Hellow world from jsx</h1>;
+
+
+// const fn = () => true;
+
+// const fn2 = () => {
+//     return true;
+// }
+
+// React Component (function component)
+// const Title = () => (
+//     <h1 className="head" tabIndex="5">
+//         Namaste Kolhapuri Dev
+//     </h1>
+// )
+
+// const HeadingComponent = () => (
+//     <div id="container">
+//         <Title />
+//         <h1 className="heading">Hello world from component</h1>
+//     </div>
+// )
+
+//// React element inside component
+const title = (
+    <h1 className="head" tabIndex="5">
+        Namaste Kolhapuri Dev
+    </h1>
+)
+
+const HeadingComponent = () => (
+    <div id="container">
+        {title}
+        <h1 className="heading">Hello world from component</h1>
+    </div>
+)
+
+// const divComponent = (<div>
+//     <HeadingComponent/>
+// </div>)
+
+const divComponent = (<div>
+    {HeadingComponent()}
+</div>)
+
+const HeadingComponent1 = () => <h1>Hellow world from component</h1>;
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(jsxHedding);
+root.render(divComponent);
