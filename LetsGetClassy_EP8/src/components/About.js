aac025435ba7d2c0d97fef3fa860ab755a704165
anshulyerudkar1 <parshwanath.ornaments.1008@gmail.com>
2026-08-30 01:07:34 +0530

    feat: implement class-based User components and integrate into About page

--- LetsGetClassy_EP8/src/components/About.js
+++ LetsGetClassy_EP8/src/components/About.js
@@ -4,16 +4,25 @@ import { Component } from "react";
 
 class About extends Component {
     constructor() {
-        super(props);
+        super();
+        console.log("Parent Class Component Constructor");
+    };
+
+    componentDidMount() {
+        console.log("Parent Class Component Did Mount");
     }
+
     render() {
+        console.log("Parent Class Component Rendered");
         return (
             <div>
                 <h1>About Us</h1>
                 <h2>This is the Namaste React Course</h2>
 
                 {/* <User name={"Anshul Yerudkar (Functional Component)"} location={"Pune (Functional Component)"}  /> */}
-                <UserClass name={"Anshul Yerudkar (Class Component)"} location={"Pune (Class Component)"}  />
+                <UserClass name={"Anshul Yerudkar (Class Component)"} location={"Pune (Class Component)"} count={"First "} />
+                <UserClass name={"Aditya Yerudkar (Class Component)"} location={"Pune (Class Component)"} count={"Second "} />
+                <UserClass name={"Amol Yerudkar (Class Component)"} location={"Pune (Class Component)"} count={"Third "} />
             </div>
         )
     }
--- LetsGetClassy_EP8/src/components/UserClass.js
+++ LetsGetClassy_EP8/src/components/UserClass.js
@@ -4,22 +4,33 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            count: 0,
-            // count2: 2,
-        }
+        // this.state = {
+        //     count: 0,
+        //     // count2: 2,
+        // }
+        console.log(this.props.count + "Child Class Component Constructor");
+    }
+
+    async componentDidMount() {
+        // console.log(this.props.count + "Child Class Component Did Mount");
+
+        const data = await fetch("https://api.github.com/users/anshulyerudkar1");
+        const json = await data.json();
+
+        console.log(this.props.count + ": " + json);
     }
     
     render() {
+        console.log(this.props.count + "Child Class Component Rendered");
         const {name, location} = this.props;
         // const {count, count2} = this.state;
-        const {count} = this.state;
+        // const {count} = this.state;
         
         return (
             <div className="user-card">
-                <h1>Count: {count} </h1>
+                {/* <h1>Count: {count} </h1> */}
                 {/* <h1>Count2: {count2} </h1> */}
-                <button onClick={() => {this.setState({count: this.state.count + 1})}}>Increase Count</button>
+                {/* <button onClick={() => {this.setState({count: this.state.count + 1})}}>Increase Count</button> */}
                 <h2>Name: {name} </h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: @ayerudkar78</h4>
